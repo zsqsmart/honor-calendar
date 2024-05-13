55dfb960da0f1d1f722a4d6f6d8283f4e3285202
smartzhao <smartzhao@tencent.com>
2024-05-13 16:26:03 +0800

    feat: init project

---
+++ .eslintrc.cjs
@@ -0,0 +1,154 @@
+const { defineConfig } = require('eslint-define-config');
+
+module.exports = defineConfig({
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+    es6: true,
+    browser: true,
+  },
+  globals: {
+    ga: 'readonly', // Google Analytics
+    cordova: 'readonly',
+    __statics: 'readonly',
+    __QUASAR_SSR__: 'readonly',
+    __QUASAR_SSR_SERVER__: 'readonly',
+    __QUASAR_SSR_CLIENT__: 'readonly',
+    __QUASAR_SSR_PWA__: 'readonly',
+    process: 'readonly',
+    Capacitor: 'readonly',
+    chrome: 'readonly',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin', 'simple-import-sort', 'vue'],
+  extends: [
+    'eslint:recommended',
+    'plugin:eslint-comments/recommended',
+    'plugin:jsonc/recommended-with-jsonc',
+    'plugin:vue/vue3-essential',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:oxlint/recommended',
+  ],
+  settings: {
+    'import/resolver': {
+      node: { extensions: ['.js', '.mjs', '.ts', '.d.ts', '.tsx'] },
+    },
+  },
+  overrides: [
+    {
+      files: ['*.json', '*.json5', '*.jsonc'],
+      parser: 'jsonc-eslint-parser',
+    },
+    {
+      files: ['tests/**'],
+      plugins: ['jest'],
+      extends: ['plugin:jest/recommended'],
+      rules: {},
+    },
+    {
+      files: ['package.json'],
+      parser: 'jsonc-eslint-parser',
+      rules: {
+        'jsonc/sort-keys': [
+          'error',
+          {
+            pathPattern: '^$',
+            order: [
+              'name',
+              'version',
+              'private',
+              'packageManager',
+              'description',
+              'type',
+              'keywords',
+              'homepage',
+              'bugs',
+              'license',
+              'author',
+              'contributors',
+              'funding',
+              'files',
+              'main',
+              'module',
+              'exports',
+              'unpkg',
+              'jsdelivr',
+              'browser',
+              'bin',
+              'man',
+              'directories',
+              'repository',
+              'publishConfig',
+              'scripts',
+              'peerDependencies',
+              'peerDependenciesMeta',
+              'optionalDependencies',
+              'dependencies',
+              'devDependencies',
+              'engines',
+              'config',
+              'overrides',
+              'pnpm',
+              'husky',
+              'lint-staged',
+              'eslintConfig',
+            ],
+          },
+          {
+            pathPattern: '^(?:dev|peer|optional|bundled)?[Dd]ependencies$',
+            order: { type: 'asc' },
+          },
+        ],
+      },
+    },
+    {
+      files: ['*.js'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+    {
+      files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
+      rules: {
+        '@typescript-eslint/consistent-type-imports': 'error',
+      },
+    },
+    {
+      files: ['*.vue'],
+      parser: 'vue-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        sourceType: 'module',
+      },
+      rules: {
+        '@typescript-eslint/consistent-type-imports': 'error',
+        'vue/multi-word-component-names': 'off',
+      },
+    },
+    {
+      files: ['packages/server/**/*.ts'],
+      rules: {
+        '@typescript-eslint/consistent-type-imports': 'off',
+      },
+    },
+    {
+      files: ['*.cjs'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+  ],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-constant-condition': 'off',
+  },
+});
---
+++ README.md
@@ -0,0 +1,14 @@
+# 荣耀日历
+
+## 技术栈
+
+- server
+
+  - nest.js
+  - mysql
+  - prisma
+
+- app
+
+  - quasar
+  - vue
